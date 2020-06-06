d2701b81984286de964b4555f7f0fb06c8e172f2
gutama8787 <gutama8787@gmail.com>
2020-06-06 14:23:03 -0400

    back button added to view book

--- src/app/ViewBookController.java
+++ src/app/ViewBookController.java
@@ -2,13 +2,19 @@ package app;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -72,6 +78,18 @@ public class ViewBookController implements Initializable {
         
 
         return libList;
+    }
+    
+
+    @FXML
+    private Button backBtn;
+
+
+    public void back(ActionEvent event) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("LibrarianSection.fxml"));
+        Scene scene = new Scene(root, 600, 600);
+        Stage stage = (Stage) tableView.getScene().getWindow();
+        stage.setScene(scene);
     }
 
 }
